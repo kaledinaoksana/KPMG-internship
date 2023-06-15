2faecf2dddb35daaf7d1b2ca12c71509ee67cf01
Oksana Kaledina <kaledinaoa@gmail.com>
2023-06-15 20:33:14 +0400

    change path of source

--- load.py
+++ load.py
@@ -1,9 +1,10 @@
 
 import pandas as pd
 import func as f
+import os
 
 def load_data():
-    path = r"/Users/oksana_kaledina/Documents/5_PORTFOLIO/KPMG/data.xlsx"
+    path = os.getcwd() + "/source/data.xlsx"
     df_transactions = pd.read_excel(path, sheet_name=1)
     df_new_cust_list = pd.read_excel(path, sheet_name=2,parse_dates=['DOB'], date_parser=lambda x: pd.to_datetime(x, format='%Y-%m-%d'))
     df_demographic = pd.read_excel(path,sheet_name=3,parse_dates=['DOB'], date_parser=lambda x: pd.to_datetime(x, format='%Y-%m-%d'))
